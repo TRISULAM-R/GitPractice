2100a5f7d52e7cd80fcd675bdd985770d22dfcd6
TrisulamR <147402726+TrisulamR@users.noreply.github.com>
2025-11-17 13:25:13 +0530

    fixed the review comments

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,7 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Hello world!");
-        System.out.println("Git Practice");
-
+        System.out.println("Added Test case");
     }
 
 }
